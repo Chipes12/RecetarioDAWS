b3e7a7bf70680278611676f8b6644d7ebccf2ac3
Maya A <m590a.roon55@gmail.com>
2021-11-23 08:19:18 -0600

    Update server.js
    cambié número  de puerto

--- Recetario/Server/server.js
+++ Recetario/Server/server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = require("./Routers/mainRouter");
 const app = express();
-const port = 3000;
+const port = 8080;
 const cors = require('cors');
 
 app.use(cors());
