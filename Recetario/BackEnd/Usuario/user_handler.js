6429a14fc3a93eb1ce572c84d40b83b41c89d318
david12343564 <david_emmi@hotmail.com>
2021-11-24 13:45:37 -0600

    Agregar nuevo user y cambios en user

--- Recetario/BackEnd/Usuario/user_handler.js
+++ Recetario/BackEnd/Usuario/user_handler.js
@@ -3,7 +3,7 @@
 const User = require("./user");
 const fs = require("fs");
 let content = fs.readFileSync("./BackEnd/Usuario/data.json");
-const users = JSON.parse(content).map(User.createFromObject);
+const users = JSON.parse(content).map(User.User.createFromObject);
 
 
 function getUsers() {
@@ -17,9 +17,9 @@ function getUserById(uid) {
 function createUser(user) {
     let p = undefined;
     if (typeof user == "string") {
-        p = User.createFromJSON(user);
+        p = User.User.createFromJSON(user);
     } else {
-        p = User.createFromObject(user);
+        p = User.User.createFromObject(user);
     }
     users.push(p);
     let newUser = JSON.stringify(user)
@@ -29,7 +29,7 @@ function createUser(user) {
 
 function updateUser(uid, updatedUser) {
     if (getUserById(uid) != undefined) {
-        User.cleanObject(updatedUser);
+        User.User.cleanObject(updatedUser);
         let index = users.findIndex(user => user._uid == uid);
         if (index > -1) {
             Object.assign(users[index], updateUser);
