4d50243955a0abc0cd3242727b1404a68f300c36
Maya A <m590a.roon55@gmail.com>
2021-11-24 15:58:59 -0600

    General update
    conexión a base de datos en server

--- Recetario/Server/server.js
+++ Recetario/Server/server.js
@@ -1,10 +1,14 @@
 "use strict";
 
+const mongo = require('mongodb').MongoClient;
+const env = require('dotenv');
 const express = require("express");
 const router = require("./Routers/mainRouter");
 const app = express();
-const port = 8080;
+const port = Number(process.env.PORT);
 const cors = require('cors');
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -14,3 +18,12 @@ app.use('/recipebook/', router);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+//conectar database
+app.use(mongo.connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log('MongoDB Connection Succeeded.');
+}));
