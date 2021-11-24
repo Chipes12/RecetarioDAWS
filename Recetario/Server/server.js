c65d0641efbd2dfa7d00ab5bc6fd12afc4ec9a7c
Maya A <m590a.roon55@gmail.com>
2021-11-24 00:43:11 -0600

    Conection DB
    Inicio de conexión a la base de datos en atlas

--- Recetario/Server/server.js
+++ Recetario/Server/server.js
@@ -5,9 +5,11 @@ const router = require("./Routers/mainRouter");
 const app = express();
 const port = 8080;
 const cors = require('cors');
+const mongodb = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+app.use(mongodb());
 app.use(router);
 
 app.listen(port, () => {
