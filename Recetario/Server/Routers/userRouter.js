2ad72a0b2b13f08d34ff5ec754c15878910ea783
david12343564 <david_emmi@hotmail.com>
2021-11-27 19:54:45 -0600

    Cambios en user para base

--- Recetario/Server/Routers/userRouter.js
+++ Recetario/Server/Routers/userRouter.js
@@ -4,52 +4,63 @@ const express = require('express');
 const router = express.Router();
 const userHandler = require('../../BackEnd/Usuario/user_handler');
 const recipeHandler = require('../../BackEnd/Receta/recipeHandler');
-const User = require("../../BackEnd/Usuario/user");
+const User = require("../../Server/models/users");
+User.find({}, res => console.log(res))
 
 router.route('/')
     .post((req, res) => {
-        let user = req.body;
+        userHandler.createUser(req, res);
+
+
+
+        /*let user = req.body;
         try {
             user = userHandler.createUser(user);
             res.status(201).json(user);
         } catch (e) {
             res.status(400).send(e.errorMessage);
-        }
+        }*/
     })
     .get((req, res) => {
 
-        if (req.query.filter == undefined) {
+        userHandler.getUsers(req, res);
+
+        /*if (req.query.filter == undefined) {
             res.status(200).json(userHandler.getUsers());
 
-        }
+        }*/
     });
 
 router.route('/:uid').get((req, res) => {
         let uid = req.params.uid;
+        userHandler.getUserById(uid, res);
+        /*let uid = req.params.uid;
         let user = userHandler.getUserById(uid);
         if (typeof user != 'undefined') res.status(200).json(user);
         else {
             res.status(404).type('text/plain').send(`Error 404 User with id: ${uid} not found`);
-        }
+        }*/
     })
     .put((req, res) => {
-        let uid = req.params.uid;
+        userHandler.updateUser(req, res);
+        /*let uid = req.params.uid;
         let userUpdated = req.body;
         if (typeof userHandler.getUserById(uid) != 'undefined') {
             userHandler.updateUser(uid, userUpdated);
             res.status(200).json(userHandler.getUserById(uid));
         } else {
             res.status(404).type('text/plain').send(`User with id: ${uid} doesn't exist`);
-        }
+        }*/
     })
     .delete((req, res) => {
-        let uid = req.params.uid;
+        userHandler.deleteUser(req, res);
+        /*let uid = req.params.uid;
         if (typeof userHandler.getUserById(uid) != 'undefined') {
             userHandler.deleteUser(uid);
             res.status(200).type('text/plain').send(`User with id: ${uid} was successfully deleted`);
         } else {
             res.status(404).type('text/plain').send(`User with id: ${uid} doesn't exist`);
-        }
+        }*/
     });
 
 router.route('/:uid/favourites').post((req, res) => {
