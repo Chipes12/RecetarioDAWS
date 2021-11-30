0cad7abb0d8a8d58b4d49511e34ec6418bda5bf6
david12343564 <david_emmi@hotmail.com>
2021-11-30 08:19:26 -0600

    Métodos de ingredientes y rutas para la base

---
+++ Recetario/Server/models/ingredients.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const mongoose = require('mongoose');
+require('dotenv').config({
+    path: 'env'
+});
+mongoose.connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const schemaIngredient = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+
+});
+
+let Ingredient = mongoose.model('ingredienteSch', schemaIngredient, 'ingredients');
+
+module.exports = Ingredient;
