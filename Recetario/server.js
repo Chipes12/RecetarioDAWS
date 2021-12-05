2f09175cc5edc9c6e4fcbcd05168a595da8bc245
Maya A <m590a.roon55@gmail.com>
2021-12-04 20:33:24 -0600

    Pages routes
    Rutas estáticas para redirigir. Cambié de lugar el server y modifiqué htmls (barra de navegación) para que tomen la ruta correcta.

--- Recetario/server.js
+++ Recetario/server.js
@@ -3,7 +3,7 @@
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const express = require("express");
-const router = require("./Routers/mainRouter");
+const router = require("./Server/Routers/mainRouter");
 const app = express();
 // const port = process.env.PORT;
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.set('port', process.env.PORT || 3000);
-app.use('/recipebook/', router);
+app.use(router);
 app.listen(app.get('port'),process.env.HOST)
 
 // app.listen(app.get('port')), () => {
